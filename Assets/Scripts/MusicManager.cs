4b4a829c675d5119c58d0aff5200cfda6b18021a
FlorianPgn <florianpgn@gmail.com>
2020-01-31 22:29:05 -0500

    Add base audio and menu

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip mainTheme;
+    public AudioClip menuTheme;
+
+    public static MusicManager Instance;
+
+    // Use this for initialization
+    void Start () {
+        Instance = this;
+	}
+
+    public void PlayMainTheme()
+    {
+        AudioManager.Instance.PlayMusic(mainTheme, 3);
+    }
+
+    public void PlayMenuTheme()
+    {
+        AudioManager.Instance.PlayMusic(menuTheme, 2);
+    }
+}
