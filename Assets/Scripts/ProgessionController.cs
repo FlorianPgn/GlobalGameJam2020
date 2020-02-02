3f88dc9dc096cbed543697bea6faec9aea898e9e
Hugo <fouraces@live.fr>
2020-02-01 21:37:10 -0500

    Progression du Zeppelin

---
+++ Assets/Scripts/ProgessionController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgessionController : MonoBehaviour
+{
+
+    public Image Zeppelin;
+    public Vector3 StartPos;
+    public Vector3 DifferencePos;
+    public Vector3 EndPos;
+    public float Timer;
+    public float TripDuration = 30f;
+    public GameObject Size;
+    
+    
+    private float _endTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = Zeppelin.transform.position;
+        Debug.Log(Size.GetComponent<RectTransform>().rect.width);
+        EndPos = new Vector3(Size.GetComponent<RectTransform>().rect.width - Zeppelin.GetComponent<RectTransform>().rect.width, Zeppelin.transform.position.y, Zeppelin.transform.position.z);
+        DifferencePos = EndPos - StartPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        if (Timer <= TripDuration)
+        {
+            Timer += Time.deltaTime;    
+            Zeppelin.transform.position = StartPos + DifferencePos * (Timer / TripDuration);
+        }
+    }
+}
